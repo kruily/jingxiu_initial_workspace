161cea45a7d4f281c28af06463343ed2c9c614b0
jingxiu1016 <48878396+jingxiu1016@users.noreply.github.com>
2022-12-16 22:26:50 +0800

    version 0.1.7

--- gateway/main.go
+++ gateway/main.go
@@ -11,23 +11,19 @@ package main
 import (
 	"gateway/config"
 	"gateway/router"
-	"gateway/services"
 )
 
 func main() {
-	//1. read config from yaml
+	// read config from yaml
 	if err := config.ConfigInit("./gateway.yaml"); err != nil {
-		panic("config file read ersor!")
+		panic("config file read error!")
 		return
 	}
 
-	// 2. start some services
-	services.SvcContext = services.NewContext(config.C)
+	// start some services
+	//services.SvcContext = services.NewContext(config.C)
 
-	// 3. start gin router
+	// start gin router
 	r := router.GinApplication()
 	router.Server(r)
-	//if err := r.Run(config.C.Gateway.Listen); err != nil {
-	//	fmt.Printf("startup service failed, err:%v\n\n", err)
-	//}
 }
